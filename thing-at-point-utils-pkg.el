4c07104fa82b9395608a6848cd72f2434a9fe260
Andreas Röhler <andreas.roehler@online.de>
2017-08-19 20:42:17 +0200

    Minor changes

--- thing-at-point-utils-pkg.el
+++ thing-at-point-utils-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "thing-at-point-utils" "1.0" "Deal with a plentitude of THINGs" '(thingatpt-utils-core) :url "https://github.com/andreas-roehler/thing-at-point-utils.git" :keywords '("convenience, lisp"))
+(define-package "thing-at-point-utils" "" "Deal with a plentitude of THINGs" '() :url "https://github.com/andreas-roehler/thing-at-point-utils.git" :keywords '("convenience"))
