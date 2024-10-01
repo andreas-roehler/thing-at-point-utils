f5f6d6acc9c0c95dc8e572b56cfdffec0a2bc9e0
Andreas Roehler <andreas.roehler@online.de>
2024-10-01 15:36:21 +0200

    ar-backward-defun-DWIM fixed
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- run-tests.sh
+++ run-tests.sh
@@ -971,23 +971,20 @@ entfernt () {
 
 if [ $IFLOCAL -eq 0 ]; then
 
-    WGE=$HOME/werkstatt/emacs-generics
+    GEN=$HOME/werkstatt/emacs-generics
     WCO=$HOME/werkstatt/thingatpt-utils-core
     ATPT=$HOME/werkstatt/thing-at-point-utils
     WNA=$HOME/werkstatt/numbers-at-point
 
-    cp -p ${WGE}/ar-subr.el ${ATPT}/ar-subr.el
-    cp -p ${WGE}/ar-beg-end.el ${ATPT}/ar-beg-end.el
-    cp -p ${WCO}/ar-thingatpt-utils-core.el ${ATPT}/ar-thingatpt-utils-core.el
-    cp -p ${WCO}/ar-thingatpt-basic-definitions.el ${ATPT}/ar-thingatpt-basic-definitions.el
-    cp -p ${WCO}/test/ar-thingatpt-setup-tests.el ${ATPT}/test/ar-thingatpt-setup-tests.el
-
-    cp -p ${WCO}/ar-subr.el ${WNA}/ar-subr.el
-    cp -p ${WGE}/ar-beg-end.el ${WNA}/ar-beg-end.el
-    cp -p ${WCO}/ar-thingatpt-basic-definitions.el ${WNA}/ar-thingatpt-basic-definitions.el
-    cp -p ${WCO}/ar-thingatpt-utils-core.el ${WNA}/ar-thingatpt-utils-core.el
-    cp -p ${ATPT}/ar-thingatpt-utils.el ${WNA}/ar-thingatpt-utils.el
-    cp -p ${WCO}/test/ar-thingatpt-setup-tests.el ${WNA}/test/ar-thingatpt-setup-tests.el
+    cp -pu ${GEN}/ar-subr.el .
+    cp -pu ${GEN}/ar-beg-end.el .
+    cp -pu ${GEN}/ar-emacs-generics-start-Zf98zM.el .
+    cp -pu ${GEN}/ar-navigate.el .
+    cp -pu ${GEN}/ar-emacs-generics-backward-forms.el .
+    cp -pu ${GEN}/ar-emacs-generics-forward-forms.el .
+    cp -pu ${WCO}/ar-thingatpt-utils-core.el .
+    cp -pu ${WCO}/ar-thingatpt-basic-definitions.el .
+    cp -pu ${WCO}/test/ar-thingatpt-setup-tests.el .
 
     while getopts 123456789abcdefghijklmnopqrstuvwxyz option
 
